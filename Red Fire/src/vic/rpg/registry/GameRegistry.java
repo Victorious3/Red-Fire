182addc4cd10d0f20a265c64777825ca61bd1eeb
Victorious3 <Minefoto0@gmail.com>
2013-11-27 18:26:53 +0100

    Version bump

--- Red Fire/src/vic/rpg/registry/GameRegistry.java
+++ Red Fire/src/vic/rpg/registry/GameRegistry.java
@@ -11,7 +11,7 @@ import vic.rpg.utils.Utils;
 
 public class GameRegistry 
 {
-	public static final String VERSION = "0.4r09";
+	public static final String VERSION = "0.4r10";
 	
 	public static Mouse mouse = new Mouse();
 	public static Key key = new Key();
